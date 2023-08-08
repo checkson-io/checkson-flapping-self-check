54c0d0688c07d720b5e5e260f50a679249658d19
Stefan Hudelmaier <hudelmaier@gmail.com>
2023-08-08 08:22:27 +0200

    Use checkson stateful feature

--- app.py
+++ app.py
@@ -2,16 +2,38 @@ import sys
 import os
 import requests
 
-URL = os.environ.get('FLAPPING_STATE_URL')
+checkson_dir = os.environ['CHECKSON_DIR']
+persistent_dir = f"{checkson_dir}/persistent"
+result_file = f"{persistent_dir}/result"
 
-r = requests.get(URL)
-r.raise_for_status()
 
-state = r.json()['state']
+def write_result(result):
+    if not os.path.exists(persistent_dir):
+        os.makedirs(persistent_dir)
 
-if state:
-    print("State is true, check successful")
-    sys.exit(0)
-else:
-    print("State is false, check unsuccessful")
-    sys.exit(1)
+    with open(result_file, "w") as f:
+        f.write(str(result))
+
+
+def read_last_result():
+    if not os.path.exists(result_file):
+        return False
+    with open(result_file) as f:
+        return "True" in f.read()
+
+
+def main():
+    last_result = read_last_result()
+    result = not last_result
+    write_result(result)
+
+    if result:
+        print("Check successful")
+        sys.exit(0)
+    else:
+        print("Check unsuccessful")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
