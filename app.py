1d5a56bfb7627efba3b353564c3a2f9454133a72
Stefan Hudelmaier <hudelmaier@gmail.com>
2023-08-08 08:41:16 +0200

    Add print statements

--- app.py
+++ app.py
@@ -9,23 +9,30 @@ result_file = f"{persistent_dir}/result"
 
 def write_result(result):
     if not os.path.exists(persistent_dir):
+        print(f"Creating {persistent_dir}")
         os.makedirs(persistent_dir)
 
+    print(f"Persisting result '{result}' to {result_file}")
     with open(result_file, "w") as f:
         f.write(str(result))
 
 
 def read_last_result():
     if not os.path.exists(result_file):
+        print(f"No result file found: {result_file}")
         return False
     with open(result_file) as f:
-        return "True" in f.read()
+        last_result = f.read().strip()
+        print(f"Persisted last result: {last_result}")
+        return "True" == last_result
 
 
 def main():
     last_result = read_last_result()
     result = not last_result
     write_result(result)
+
+    print(f"Last result: {last_result}, new result: {result}")
 
     if result:
         print("Check successful")
